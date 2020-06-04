c484de2e9adf70ba7b01e1b92dcf2c1b074c4448
Yue KANG <yue@netroworx.com>
2020-06-04 23:11:59 +1000

    fix bugs in lint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render} from '@testing-library/react';
-import App from './App';
+
+import Dashboard from './pages/Dashboard';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Dashboard />);
   const linkElement = getByText(/home/i);
   
   expect(linkElement).toBeInTheDocument();
