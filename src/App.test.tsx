df1882109bab42bda5d42133c200e7a240584e49
Yue KANG <yue@netroworx.com>
2020-06-06 14:12:39 +1000

    fix test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render} from '@testing-library/react';
 
-import Dashboard from './pages/Dashboard';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<Dashboard />);
-  const linkElement = getByText(/home/i);
+import Test from "./test"
+
+test('render test', () => {
+  const { getByText } = render(<Test/>);
+  const linkElement = getByText(/test/i);
   
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/test.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {Component} from "react";
+class test extends Component {
+
+    render(){
+
+        return <div>
+                <h1>test</h1>
+            </div>
+
+    };
+}
+
+
+export default test;
