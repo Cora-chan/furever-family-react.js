50c62dc01ed383afeb759ba828e948f6154abbc5
Yue KANG <yue@netroworx.com>
2020-06-15 18:29:42 +1000

    Modification in Add pets page

--- src/pages/AddPets.js
+++ src/pages/AddPets.js
@@ -1,8 +1,7 @@
 import React from "react"
 import{Link} from "react-router-dom"
 import IconButtonBack from "../components/Buttons/IconButtonBack"
-import AddPetsHeader from "../components/Headers/AddPetsHeader"
-import ListInAddPets from "../components/Lists/ListInAddPets"
+
 
 import ContainedButtonCancel from "../components/Buttons/ContainedButtonCancel"
 import ContainedButtonMedium from "../components/Buttons/ContainedButtonMedium"
@@ -10,7 +9,7 @@ import ContainedButtonMedium from "../components/Buttons/ContainedButtonMedium"
 function AddPets(){
     return(
 <div>
-        
+<IconButtonBack component={Link}to="/manage/pets"/>       
        
         
 <ContainedButtonCancel color="error" caption="Cancel" component={Link}to="/manage/pets"  />
